6e0596a6957f46d372d89e1ef150bf37e86a89ff
Tamer Tosun <tamer.tosun@campana-schott.com>
2023-06-28 17:01:14 +0200

    init cstato

--- enterprise-scale/modules/management/main.tf
+++ enterprise-scale/modules/management/main.tf
@@ -41,7 +41,7 @@ module "alz" {
   # Base module configuration settings
   root_parent_id   = data.azurerm_client_config.current.tenant_id
   root_id          = var.root_id
-  default_location = var.primary_locations
+  default_location = var.primary_location
 
   # Disable creation of the core management group hierarchy
   # as this is being created by the core module instance
