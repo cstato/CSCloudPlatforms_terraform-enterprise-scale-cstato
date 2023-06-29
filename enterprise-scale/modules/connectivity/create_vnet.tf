764574ba4f00368cbce54cbd73a2702140282a3b
Tamer Tosun <tamer.tosun@campana-schott.com>
2023-06-29 10:03:45 +0200

    vnet

---
+++ enterprise-scale/modules/connectivity/create_vnet.tf
@@ -0,0 +1,26 @@
+resource "azurerm_resource_group" "example" {
+  name     = "TEST-RG"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "TEST-NETWORK"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = []
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
